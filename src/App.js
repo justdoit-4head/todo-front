90bc763ffd0a1390c695ed66370a0e1f3d0a3c81
justdoit-4head <officialsoap11@gmail.com>
2023-02-03 00:03:56 +0530

    added backend url

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-const api_base = 'http://localhost:5000';
+const api_base = 'https://todo-back-vklp.onrender.com';
 
 function App() {
 	const [todos, setTodos] = useState([]);
